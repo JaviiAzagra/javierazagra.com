d10f9e5d2c9cad7b53c2e18c7be03f5a8a7c0eda
JaviiAzagra <javiazagra12@gmail.com>
2024-02-09 01:39:56 +0100

    FEAT: added lng on localstorage

--- src/Components/i18n/i18n.js
+++ src/Components/i18n/i18n.js
@@ -4,32 +4,37 @@ import { initReactI18next } from "react-i18next";
 import en from "./en.json";
 import es from "./es.json";
 import de from "./de.json";
-import I18nextBrowserLanguageDetector from "i18next-browser-languagedetector";
 
-i18n
-  .use(initReactI18next)
-  .use(I18nextBrowserLanguageDetector)
-  .init({
-    resources: {
-      en: {
-        translation: en,
-      },
-      es: {
-        translation: es,
-      },
-      de: {
-        translation: de,
-      },
+const LANGUAGE_KEY = "appLanguage";
+
+const storedLanguage = localStorage.getItem(LANGUAGE_KEY);
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
     },
-    fallbackLng: "en",
-    debug: true,
-    interpolation: {
-      escapeValue: false,
+    es: {
+      translation: es,
     },
-    detection: {
-      order: ["localStorage", "navigator"],
-      caches: ["localStorage"],
+    de: {
+      translation: de,
     },
-  });
+  },
+  lng: storedLanguage || "en",
+  fallbackLng: "es",
+  debug: true,
+  interpolation: {
+    escapeValue: false,
+  },
+  detection: {
+    order: ["localStorage", "navigator"],
+    caches: ["localStorage"],
+  },
+});
+
+i18n.on("languageChanged", (lng) => {
+  localStorage.setItem(LANGUAGE_KEY, lng);
+});
 
 export default i18n;
