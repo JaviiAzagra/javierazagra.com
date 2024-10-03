29b2a0c9263f5597528a6796cb162fc6f13aee68
JaviiAzagra <javiazagra12@gmail.com>
2024-10-03 15:33:05 +0200

    Refactor experience cards rendering in Experience component

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -4,56 +4,49 @@ import "./Experience.scss";
 
 const Experience = () => {
   const { t } = useTranslation();
+  const experience = [
+    {
+      date: "2023",
+      date2: "Present",
+      title: "Full Stack Developer",
+      link: "javierazagra.com",
+      description: t("Experience1"),
+    },
+    {
+      date: "May",
+      date2: "June 2023",
+      title: "IT Technician",
+      link: "primaverasound.com",
+      description: t("Experience2"),
+    },
+    {
+      date: "April",
+      date2: "June 2021",
+      title: "IT Technician",
+      link: "alten.es",
+      description: t("Experience3"),
+    },
+  ];
   return (
     <div className="experience" id="experience">
       <div className="experience--title"></div>
 
       <div className="experience__cards">
-        <div className="experience__cards--card">
-          <div className="experience__cards--card__left">
-            <p>2023</p>
-            <p>Present</p>
+        {experience.map((experience) => (
+          <div className="experience__cards--card">
+            <div className="experience__cards--card__left">
+              <p>{experience.date}</p>
+              <p>{experience.date2}</p>
+            </div>
+            <div className="experience__cards--card__right">
+              <h1>{experience.title}</h1>
+              <a href={experience.link} target="_blank" rel="noreferrer">
+                {experience.link}
+              </a>
+              <p>{experience.description}</p>
+            </div>
           </div>
-          <div className="experience__cards--card__right">
-            <h1>Full Stack Developer</h1>
-            <a href="https://javierazagra.com" target="_blank" rel="noreferrer">
-              javierazagra.com
-            </a>
-            <p>{t("Experience1")}</p>
-          </div>
-        </div>
-
-        <div className="experience__cards--card">
-          <div className="experience__cards--card__left">
-            <p>May</p>
-            <p>June 2023</p>
-          </div>
-          <div className="experience__cards--card__right">
-            <h1>IT Technician</h1>
-            <a
-              href="https://www.primaverasound.com/es/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              primaverasound.com
-            </a>
-            <p>{t("Experience2")}</p>
-          </div>
-        </div>
-
-        <div className="experience__cards--card">
-          <div className="experience__cards--card__left">
-            <p>April</p>
-            <p>June 2021</p>
-          </div>
-          <div className="experience__cards--card__right">
-            <h1>IT Technician</h1>
-            <a href="https://www.alten.es/" target="_blank" rel="noreferrer">
-              alten.es
-            </a>
-            <p>{t("Experience3")}</p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
